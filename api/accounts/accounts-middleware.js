08c1d10007fd184f72b8d6c828896f9a82261026
Nora C <nocorser@gmail.com>
2021-07-18 16:56:18 -0500

    checkAccountId

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,5 @@
+const Account = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   console.log('some kinda middleware is active here');
@@ -14,10 +16,16 @@ exports.checkAccountNameUnique = (req, res, next) => {
 
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('some kinda middleware is active here');
-
-  next()
-
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id)
+    if (!account) {
+      next({ status: 404, message: 'not found'})
+    } else {
+      req.account = account
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
