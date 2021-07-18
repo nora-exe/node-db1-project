ce905a3f138c1cd7df2027d8dea05bcffee525d2
Nora C <nocorser@gmail.com>
2021-07-18 17:38:35 -0500

    checkAccountPayload

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,31 @@
 const Account = require('./accounts-model');
 
+/**
+  - If either name or budget are undefined, return `{ message: "name and budget are required" }`
+  - If name is not a string, return `{ message: "name of account must be a string" }`
+  - If the _trimmed_ name is shorter than 3 or longer than 100, return `{ message: "name of account must be between 3 and 100" }`
+  - If budget is not a number, return `{ message: "budget of account must be a number" }`
+  - If budget is a negative number or over one million, return  `{ message: "budget of account is too large or too small" }`
+*/
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('some kinda middleware is active here');
-
-  next()
-
+  const error = { status: 400 }
+  const {name, budget} = req.body
+  if (name === undefined || budget === undefined) {
+    error.message = 'name and budget are required'
+    next(error)
+  } else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+    next(error)
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = 'name of account must be between 3 and 100'
+    next(error)
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    error.message = 'budget of account must be a number'
+    next(error)    
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
+    next(error)
+  } else next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
